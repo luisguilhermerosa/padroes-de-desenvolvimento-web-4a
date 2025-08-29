9063752fa63278db3e5446cad506efbd0e36a539
luisguilhermerosa <luisguigo2@gmail.com>
2025-08-29 01:40:04 +0000

    28/08

--- projeto/src/main/java/entidades/Produto.java
+++ projeto/src/main/java/entidades/Produto.java
@@ -1,15 +1,30 @@
 package entidades;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Categoria {
+public class Produto {
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer codigo;
 
+
     public String nome;
+
+    public double preco;
+
+    public int quantidade;
+
+    public LocalDate validade;
+    
+    
+    
+
+    
 }
