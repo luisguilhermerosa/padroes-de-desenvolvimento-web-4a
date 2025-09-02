db5e876df8725d6c3b22e8bee520b65a7d3bd904
luisguilhermerosa <luisguigo2@gmail.com>
2025-09-02 01:08:25 +0000

    01/09

--- projeto/src/main/resources/import.sql
+++ projeto/src/main/resources/import.sql
@@ -1,3 +1,4 @@
+-- tabela Categoria
 insert into Categoria (nome) values ('Bebidas');
 insert into Categoria (nome) values ('Frutas');
 insert into Categoria (nome) values ('Carnes');
@@ -8,3 +9,47 @@ insert into Categoria (nome) values ('Higiene Pessoal');
 insert into Categoria (nome) values ('Limpeza');
 insert into Categoria (nome) values ('Padaria');
 insert into Categoria (nome) values ('Doces');
+
+-- tabela Fabricante
+
+insert into Fabricante (nome) values ('Ambev');
+insert into Fabricante (nome) values ('Fazenda da Toca');
+insert into Fabricante (nome) values ('Friboi');
+insert into Fabricante (nome) values ('Sadia');
+insert into Fabricante (nome) values ('Piracanjuba');
+insert into Fabricante (nome) values ('Sadia');
+insert into Fabricante (nome) values ('Pomarola');
+insert into Fabricante (nome) values ('Natura');
+insert into Fabricante (nome) values ('Ype');
+insert into Fabricante (nome) values ('Padaria');
+insert into Fabricante (nome) values ('Nestle');
+
+-- tabela Cliente
+
+insert into Cliente (nome,cpf) values ('Lucas', '103.573.847-04');
+insert into Cliente (nome,cpf) values ('Mariana', '234.567.346-04');
+insert into Cliente (nome,cpf) values ('Joao', '154.127.980-27');
+insert into Cliente (nome,cpf) values ('Ana', '821.643.230-60');
+insert into Cliente (nome,cpf) values ('Rafael', '103.456.087-65');
+insert into Cliente (nome,cpf) values ('Beatriz', '103.436.678-54');
+insert into Cliente (nome,cpf) values ('Bruno', '456.578.846-67');
+insert into Cliente (nome,cpf) values ('Camila', '134.465.436-04');
+insert into Cliente (nome,cpf) values ('Gustavo', '345.567.854-04');
+insert into Cliente (nome,cpf) values ('Pedro', '167.648.784-54');
+insert into Cliente (nome,cpf) values ('Matheus', '145.773.654-04');
+
+
+-- tabela Funcionario
+
+insert into Funcionario (nome,cpf) values ('Pedro', '345.987.346-93');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Carlos Henrique Pereira', '123.456.789-09');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Renata Souza Lima', '987.654.321-00');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Thiago Moura Reis', '741.852.963-20');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Paula Nogueira Silva', '852.963.741-01');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Bruno Macedo Costa', '369.258.147-05');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Fernanda Teixeira Rocha', '258.147.369-90');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Diego Figueiredo Ramos', '147.369.258-88');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Luciana Carvalho Monteiro', '963.741.852-77');
+INSERT INTO Funcionario (nome, cpf) VALUES ('Vinícius Almeida Rocha', '159.357.486-60');
+
+
